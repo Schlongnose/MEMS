bcbbe577a5bca2aac1d18847146a9c5baebd1558
Schlongnose <101610834+Schlongnose@users.noreply.github.com>
2022-12-21 23:10:56 -0500

    Add project files.

---
+++ MEMS/MemoryBoard.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEMS
+{
+    // constructor:
+    //  * create the board with cards in order
+    //  * shuffle the cards
+    // ability (function) to print board (all unmasked)
+    // ability (function) to print board (all masked)
+    public class MemoryBoard
+    {
+        private int _iNumOfCards;
+
+        private List<int> board;
+        private List<bool> facedown;
+
+        public MemoryBoard(int iNumOfCards)
+        {
+            ValidateInput(iNumOfCards);
+
+            board = new List<int>();
+            _iNumOfCards = iNumOfCards;
+            facedown = new List<bool>();
+            InitializeBoard();
+            Shuffle();
+        }
+
+        private void ValidateInput(int iNumOfCards)
+        {
+            if (iNumOfCards <= 9)
+            {
+                throw new Exception("Memory Board must have at least 10 cards");
+            }
+
+            if (iNumOfCards > 52)
+            {
+                throw new Exception("Memory Board must have less 52 cards");
+            }
+
+            // if iNumOfCards is an odd number, error or throw exception
+            if (iNumOfCards % 2 == 1)
+            {
+                throw new Exception("Memory Board must have even number of cards");
+            }
+        }
+
+        private void InitializeBoard()
+        {
+            var card_num = 1;
+            while (board.Count < _iNumOfCards)
+            {
+                board.Add(card_num);
+                facedown.Add(true);
+                board.Add(card_num);
+                facedown.Add(true);
+                card_num++;                 
+            }
+           
+            return;
+        }
+
+        private void Shuffle()
+        {
+            // Creating a object
+            // for Random class
+            Random random = new Random();
+            int last_position = board.Count - 1;
+            // Start from the last element and
+            // swap one by one. We don't need to
+            // run for the first element
+            // that's why i > 0
+            for (int i = 0; i <= last_position; i++)
+            {
+
+                // Pick a random index
+                // from 0 to i
+                int j = random.Next(0, i + 1);
+
+                // Swap arr[i] with the
+                // element at random index
+                int temp = (int)board[i];
+                board[i] = board[j];
+                board[j] = temp;
+                // PrintArray(arr);
+                //Console.WriteLine();
+            }
+        }
+
+        //public void Open(int position)
+        //{
+        //    facedown[position] = false;
+        //}
+        public void Remove(int position, int position2)
+        {
+            int maxPosition =Math.Max(position, position2);
+            int minPosition =Math.Min(position, position2);
+            board.RemoveAt(maxPosition);
+            board.RemoveAt(minPosition);
+            facedown.RemoveAt(maxPosition);
+            facedown.RemoveAt(minPosition);
+        }
+
+        public void Open(int position, int position2)
+        {
+            facedown[position] = false;
+            facedown[position2] = false;
+        }
+        public bool IsMatch(int position, int position2)
+        {
+           var result = (board[position] == board[position2]);
+            return result;
+        }
+        public bool IsBoardEmpty()
+        {
+            var result = board.Count == 0;
+            return result;
+        }
+        //fix when to print value vs when to print maskchar in the for loop
+        public void Print(char maskchar)
+        {
+            for (int i = 0; i < board.Count; i++)
+            {
+                if (((bool)facedown[i]) == true)
+                {
+                    Console.Write(maskchar);
+                }
+
+                if (((bool)facedown[i]) == false)
+                {
+                    Console.Write(board[i]);
+                }
+
+                //Console.Write(board[i]);
+                //Console.Write(facedown[i]);
+                //if (i != _iNumOfCards)
+                //{
+                Console.Write("  ");
+
+           
+
+                //}
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            
+        }
+
+        public void CloseAll()
+        {
+            for(int i = 0; i < facedown.Count; i++)
+            {
+                facedown[i] = true;
+            }
+        }
+
+        public void OpenAll()
+        {
+            for (int i = 0; i < facedown.Count; i++)
+            {
+                facedown[i] = false;
+            }
+        }
+    }
+}
---
+++ MEMS/Program.cs
@@ -0,0 +1,239 @@
+﻿using MEMS;
+using System;
+using System.Collections;
+using System.ComponentModel;
+
+public class Program
+{
+    /*public static void Main1()
+    {
+        //int player1 = 0;
+        //int player2 = 0;
+
+        //ArrayList arrayList = new ArrayList();
+        Console.WriteLine("Welcome to Billy's guess that number game choose a number");
+
+        Random random = new Random();
+        int randomNumber = random.Next(1, 11);
+        int numberOfGuesses = 3;
+        bool gameOver = false;
+        int guess;
+
+
+        while(gameOver == false)
+        {
+            guess = Convert.ToInt32(Console.ReadLine());
+            numberOfGuesses--;
+
+            if(guess != randomNumber && numberOfGuesses == 0)
+            {
+                Console.WriteLine("You Lose GG");
+                gameOver = true;
+            }
+            else if(guess == randomNumber)
+            {
+                Console.WriteLine("You Win");
+                gameOver = true;
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Too Much!");
+            }
+            else if (guess < randomNumber)
+            {
+                Console.WriteLine("Too Small");
+            }
+
+        }
+        Console.ReadLine();
+
+    }
+    */
+    public static int Getinput()
+    {
+        var input = Console.ReadLine();
+        var result = int.Parse(input);
+        return result;
+    }
+
+    public static void PrintArray(ArrayList ar)
+    {
+        int array_Size = ar.Count;
+        int last_position = array_Size - 1;
+        Console.Write("Cards: ");
+        for (int i = 0; i <= last_position; i++)
+        {
+            Console.Write(ar[i]); // Print out the content of the arraylist at position i.
+            if (i != last_position) // If I am not at the last position, print a " " space to separate the numbers.
+            {
+                Console.Write("  ");
+            }
+        }
+    }
+
+    public static void Print_MaskedArray(ArrayList ar)
+    {
+        int array_Size = ar.Count;
+        int last_position = array_Size - 1;
+        Console.Write("Cards: ");
+        for (int i = 0; i <= last_position; i++)
+        {
+            Console.Write("*"); // Print out the content of the arraylist at position i.
+            if (i != last_position) // If I am not at the last position, print a " " space to separate the numbers.
+            {
+                Console.Write("  ");
+            }
+        }
+    }
+
+    public static void Print_SelectedCards(ArrayList ar, int pos1, int pos2)
+    {
+        int array_Size = ar.Count;
+        int last_position = array_Size - 1;
+        Console.Write("Cards: ");
+        for (int i = 0; i <= last_position; i++)
+        {
+            if (i == pos1 || i == pos2) // If I am the selected cards, un-mask them and show.
+            {
+                Console.Write(ar[i]);
+            }
+            else
+            {
+                Console.Write("*"); // Mask the card content.
+            }
+
+            if (i != last_position) // If I am not at the last position, print a " " space to separate the numbers.
+            {
+                Console.Write("  ");
+            }
+        }
+    }
+
+    static void ShuffleCards(ArrayList arr)
+    {
+        // Creating a object
+        // for Random class
+        Random r = new Random();
+        int array_Size = arr.Count;
+        int last_position = array_Size - 1;
+        // Start from the last element and
+        // swap one by one. We don't need to
+        // run for the first element
+        // that's why i > 0
+        for (int i = 0; i <= last_position; i++)                                              
+        {
+
+            // Pick a random index
+            // from 0 to i
+            int j = r.Next(0, i + 1);
+
+            // Swap arr[i] with the
+            // element at random index
+            int temp = (int)arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            // PrintArray(arr);
+            //Console.WriteLine();
+        }
+    }
+
+    public static void Main()
+    {
+        try
+        {
+            /*  Console.WriteLine("Welcome to Billy's Memory Game Match The Following Numbers");
+              int player1 = 0;
+              int player2 = 0;
+              bool gameOver = false;
+              bool player1turn = false;
+              bool player2turn = false;
+
+              ArrayList arrlist = new();
+              const int ARRAY_SIZE = 18;
+              int card_num = 1;
+
+              for (int i = 0; i < ARRAY_SIZE; i += 2)
+              {
+                  arrlist.Add(card_num);
+                  arrlist.Add(card_num);
+                  card_num++;
+              }
+              ShuffleCards(arrlist);
+              Print_MaskedArray(arrlist);
+              Console.WriteLine("Player 1's turn. please select 2 guesses");
+
+
+
+
+
+                  int guess1 = Getinput();
+                  int guess2 = Getinput();
+
+              while (!gameOver)
+              { 
+                  if (guess1 < 0 || guess1 > 19)
+                  {
+                      Console.WriteLine("Please choose numbers between 0 - 18");
+                      guess1 = Getinput;
+
+                  }
+                  if (guess2 < 0 || guess2 > 19)
+                  {
+                      Console.WriteLine("Please choose numbers between 0 - 18");
+                      guess2 = Getinput;
+                  }
+              }
+
+
+
+
+
+              //PrintArray(arrlist);
+
+              Print_SelectedCards(arrlist, guess1-1, guess2-1);
+              // Console.Clear(); // Clear the screen, basically a refresh
+
+              */
+            
+            MemoryBoard memoryBoard = new MemoryBoard(10);
+            memoryBoard.Print('%');
+            
+            while(!memoryBoard.IsBoardEmpty()) //Same as != True
+            {
+                Console.WriteLine("Player 1 choose a card");
+                int guess1 = Getinput();
+                Console.WriteLine("Player 1 choose another card");
+                int guess2 = Getinput();
+                bool playerOneTurn = true;
+                memoryBoard.Open(guess1, guess2);
+                memoryBoard.Print('%');
+                bool checkingIsMatch = memoryBoard.IsMatch(guess1, guess2);
+                if (checkingIsMatch == true)
+                {
+                    memoryBoard.Remove(guess1, guess2);
+                }
+                else
+                {
+                    memoryBoard.CloseAll();
+                }
+            }
+           
+            //memoryBoard.OpenAll();
+            ////memoryBoard.PrintMaasked('$');
+            //memoryBoard.Print('*');
+            //memoryBoard.Open(3, 7);//lets pretend the player elects to open the index 3
+            //memoryBoard.Open(10, 12); 
+            //memoryBoard.Print('*');
+            //memoryBoard.CloseAll();
+            //memoryBoard.Print('*');
+            //memoryBoard.Open(5, 8);
+            //memoryBoard.Print('*');
+            //var ismatch = memoryBoard.IsMatch(9, 9);
+            //Console.WriteLine(ismatch);
+        }
+        catch (Exception x)
+        {
+
+        }
+    }
+}
